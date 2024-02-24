ffbf49738914a9260daca25bc061ce27015bb155
Akemi <ake3desuyo@gmail.com>
2024-02-24 22:54:34 +0200

    Initial commit

---
+++ Assets/UnityTechnologies/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
